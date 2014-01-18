19e1f935d4aed61beedd863990f79e7e152493bf
John D. Storey <john@openapparatus.com>
2014-01-18 12:53:58 -0700

    re-organizes file structure, adds left column toggle animations

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(function() {
+
+		$('#sidebar-toggle-right').click(function(){
+			// console.log('hi');
+			$('body').toggleClass('sidebar-open-right');
+			
+
+		});	
+
+		$('#sidebar-toggle-left').click(function(){
+			// console.log('hi');
+			$('body').toggleClass('sidebar-open-left');
+		});			
+
+
+	
+
+
+	
+	
+});
