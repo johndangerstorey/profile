f2c1ccf2be86dc398ae346a47bfe9f87a321d069
John D. Storey <john@openapparatus.com>
2014-01-18 20:19:08 -0700

    adds edit on demand functionality with save button

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,37 @@ $(function() {
 		$('#sidebar-toggle-right').click(function(){
 			// console.log('hi');
 			$('body').toggleClass('sidebar-open-right');
-			
-
 		});	
 
 		$('#sidebar-toggle-left').click(function(){
 			// console.log('hi');
 			$('body').toggleClass('sidebar-open-left');
-		});			
+		});	
+
+		 // $('.edit').editable('http://www.example.com/save.php');
+		$('body').on('click', '.edit', function() {
+			var oldEl = $(this),
+				newEl = $('<input class="edit-new" placeholder="" />');
+				
+
+				// $('.save-input').prepend( " | " );
+				$('.save-input').show();
+				
+
+				oldEl.replaceWith(newEl);
+				newEl.focus();
+				
+
+				$('.save-input').on('click', function(e) {
+					newEl = $('.edit-new').val();
+					// alert(newEl);
+					// newEl.replaceWith(oldEl);
+					e.preventDefault();
+					$('.save-input').hide();
+				});
+		});
+
+
 
 
 	
